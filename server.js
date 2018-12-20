568395ee4643f42407381107bcfc445ac94b12e4
unknown <jjernigan1065@gmail.com>
2018-12-20 16:13:51 -0500

    staged express server file and made a client folder for the front end. added all react app files to client.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require(express);
+const mongoose = require(mongoose);
+
+// port variable (local or production)
+
+// connect to database (local or production)
+
+// routes or import from route module
+
+// connect to server (local or production)
