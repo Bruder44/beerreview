95b814a49bf9ea3a1bed69ecca1adfe8f46fe3b0
unknown <jjernigan1065@gmail.com>
2018-12-20 16:34:31 -0500

    outlined controller, server file, and routes needed on the server side."

---
+++ controller/beerController.js
@@ -0,0 +1,12 @@
+// find all beers controller when route hit
+
+// retrieve beer by category when route hit
+
+// retrieve beer by user input when route hit
+
+// create new beer with pairing notes and info when route hit
+
+// update beer with new pairing notes when route hit
+
+// update public score when route hit (contains logic for finding score out of 5)
+
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// get route to retrieve all beers from database
+
+// get route to retrieve beers by category
+
+// get route to retrieve beers by user input
+
+// create new beers with pairing notes and info
+
+// update beers with new pairing notes
+
+// update public score 
