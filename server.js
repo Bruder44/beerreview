a8e88c266fb41266ea08765cca57912cb4ed9aee
unknown <jjernigan1065@gmail.com>
2018-12-20 17:41:35 -0500

    server spins up and listens. waiting on development environment db to test connection. McCloskey is setting up basic controller. Bruder is handling our database info and seeding

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const express = require(express);
-const mongoose = require(mongoose);
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// require('dotenv').load();
 
 // port variable (local or production)
+const PORT = process.env.PORT || 3001;
+
+// initialize express object
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// Promise = mongoose.Promise;
+
+
+
 
 // connect to database (local or production)
-
+// mongoose.connect(MONGODO_URI, err => {
+//     if (err) console.error(err);
+//     else console.log(`Database Connected!`);
+// })
 // routes or import from route module
 
 // connect to server (local or production)
+app.listen(PORT, err => {
+    if (err) console.error(err);
+    else console.log(`Server listening on port ${PORT}`);
+});
