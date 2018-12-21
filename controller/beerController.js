8fadd816868ab9a3886de8854f2c05c98450b622
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-12-20 19:48:25 -0500

    updated controller

--- controller/beerController.js
+++ controller/beerController.js
@@ -7,19 +7,52 @@ module.exports = {
         console.log("Loading them all");
         db.Beer
             .find({})
-            .sort({ style: -1})
+            .sort({ style: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-    }
+    },
 
 // retrieve beer by category when route hit
+    findByStyle: (req, res) => {
+        console.log(`Listing  all available ${req.body.style}s in your area.`);
+        db.Beer
+            .find({ style: req.body.style })
+            .sort({ name: -1})
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
 
 // retrieve beer by user input when route hit
+    findPairing: (req, res) => {
+        console.log(`Finding beers to pair with ${req.body.pairingNotes}...`);
+        db.Beer
+            .find({ pairingNotes: req.body.pairingNotes })
+            .sort({ name: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
 
 // create new beer with pairing notes and info when route hit
+    newBeer: (req, res) => {
+        console.log("adding beer to database..");
+        db.Beer
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
 
 // update beer with new pairing notes when route hit
+    addNotes: (req, res) => {
+        console.log(`updating ${req.body.id} with new notes`);
+        db.Beer
+            .findOneAndUpdate({ _id: req.body.id },
+                { $push: {pairingNotes: req.body.pairingNotes} }
+            )
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
 
 // update public score when route hit (contains logic for finding score out of 5)
+    
 
 }
