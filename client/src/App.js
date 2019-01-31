2f7e2a285abad2516a0dcae6e0be49cf135fe21f
unknown <jjernigan1065@gmail.com>
2019-01-31 16:49:27 -0500

    fixed merge conflicts from pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import Header from './components/Header';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
-
-=======
 // import Header from './components/Header'
 import Carousel from './components/Carousel/Carousel'
 // import Footer from './components/Footer'
 // import logo from './logo.svg';
 // import './App.css';
->>>>>>> 6d3ca0323fcaa5c4326d27573b2237d505b7e666
 
 class App extends Component {
   render() {
