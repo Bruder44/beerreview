8d8727c0b419ddd4889a33e0535953b10f4effe3
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2019-02-22 18:30:35 -0500

    adjusted a weird indentation

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-         <Header /> 
+        <Header /> 
         <Carousel />
-         <Footer /> 
+        <Footer /> 
       </div>
     );
   }
