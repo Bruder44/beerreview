f61b8139c42d700d56c1c6626f9e11a37f6cbf2b
Matthew Bruder <mbruder@students.kennesaw.edu>
2019-03-20 17:02:44 -0400

    update 3-20-3

---
+++ client/src/components/UploadForm/UploadForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import API from '../../utils/API';
+
+class UploadForm extends Component {
+    state = {
+        beer: "",
+        brewer: "",
+        style: "",
+        pairingNotes: [],
+        imgFilePath: "",
+        score: "",
+        userRating: "",
+        numberOfVotes: "",
+        availableIn: [],
+        abv: "",
+
+    }
+}
